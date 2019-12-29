a49cc0f5423ef2691cb2d2db32e0ad34f5e8bcb9
Daichi Hioki <hiokidaichi@gmail.com>
2019-12-29 22:52:15 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,50 @@
+use std::collections::HashMap;
+
 fn main() {
+    t04();
     t03();
     t02();
     t01();
     t00();
+}
+
+// 04. 元素記号
+// "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can."という文を単語に分解し，1, 5, 6, 7, 8, 9, 15, 16, 19番目の単語は先頭の1文字，それ以外の単語は先頭に2文字を取り出し，取り出した文字列から単語の位置（先頭から何番目の単語か）への連想配列（辞書型もしくはマップ型）を作成せよ．
+fn t04() {
+    let input = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.";
+
+    let mut answer: Vec<HashMap<String, usize>> = vec![];
+
+    let mut cs = input.chars();
+
+    let mut index: usize = 1;
+
+    while cs.as_str().len() > 0 {
+        let s: String = cs.by_ref().take_while(|c| c.is_alphanumeric()).collect();
+
+        if s.is_empty() {
+            continue;
+        }
+
+        let num_take = match index {
+            1 | 5 | 6 | 7 | 8 | 9 | 15 | 16 | 19 => 1,
+            _ => 2,
+        };
+
+        let word: String = s.chars().take(num_take).collect();
+
+        let mut map: HashMap<String, usize> = HashMap::new();
+        map.insert(word, index);
+
+        answer.push(map);
+
+        index += 1;
+    }
+
+    assert_eq!(
+        format!("{:?}", answer),
+        r#"[{"H": 1}, {"He": 2}, {"Li": 3}, {"Be": 4}, {"B": 5}, {"C": 6}, {"N": 7}, {"O": 8}, {"F": 9}, {"Ne": 10}, {"Na": 11}, {"Mi": 12}, {"Al": 13}, {"Si": 14}, {"P": 15}, {"S": 16}, {"Cl": 17}, {"Ar": 18}, {"K": 19}, {"Ca": 20}]"#
+    );
 }
 
 // 03. 円周率
